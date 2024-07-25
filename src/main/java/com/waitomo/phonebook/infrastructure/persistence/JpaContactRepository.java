31674b09bcb97e020751cfb1c11a6a40970d0c1f
Namibo06 <namibo654@gmail.com>
2024-07-25 19:20:47 -0300

    feat: JpaContactRepository

---
+++ src/main/java/com/waitomo/phonebook/infrastructure/persistence/JpaContactRepository.java
@@ -0,0 +1,10 @@
+package com.waitomo.phonebook.infrastructure.persistence;
+
+import com.waitomo.phonebook.domain.entities.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaContactRepository extends JpaRepository<Contact,Long>{
+
+}
