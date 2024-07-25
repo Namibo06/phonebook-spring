e7944b11775e9998f76765041d01a07911ab1fca
Namibo06 <namibo654@gmail.com>
2024-07-25 20:26:19 -0300

    refactor: remove log

--- src/main/java/com/waitomo/phonebook/application/usecases/CreateContactUseCase.java
+++ src/main/java/com/waitomo/phonebook/application/usecases/CreateContactUseCase.java
@@ -4,15 +4,11 @@ import com.waitomo.phonebook.application.services.ContactService;
 import com.waitomo.phonebook.domain.entities.Contact;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Component
 public class CreateContactUseCase {
     @Autowired
     private ContactService contactService;
-
-    private static final Logger log = LoggerFactory.getLogger(CreateContactUseCase.class);
 
     public Contact execute(String name,String number){
         Contact contact = new Contact();
