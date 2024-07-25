7adfaa9d9de789c8ffb721a8ac226398ebba5d99
Namibo06 <namibo654@gmail.com>
2024-07-25 19:21:04 -0300

    feat: ContactRepositoryImpl

---
+++ src/main/java/com/waitomo/phonebook/infrastructure/persistence/impl/ContactRepositoryImpl.java
@@ -0,0 +1,59 @@
+package com.waitomo.phonebook.infrastructure.persistence.impl;
+
+import com.waitomo.phonebook.domain.entities.Contact;
+import com.waitomo.phonebook.domain.repositories.ContactRepository;
+import com.waitomo.phonebook.infrastructure.persistence.JpaContactRepository;
+import com.waitomo.phonebook.infrastructure.web.dtos.ContactResponseDTO;
+import com.waitomo.phonebook.infrastructure.web.dtos.MessageStatusResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class ContactRepositoryImpl implements ContactRepository {
+
+    private final JpaContactRepository jpaContactRepository;
+
+    @Autowired
+    public ContactRepositoryImpl(JpaContactRepository jpaContactRepository) {
+        this.jpaContactRepository = jpaContactRepository;
+    }
+
+    @Override
+    public Contact save(Contact contact) {
+        return jpaContactRepository.save(contact);
+    }
+
+    @Override
+    public Optional<Contact> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Contact> findByNumberPhone(String phone) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Page<ContactResponseDTO> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public MessageStatusResponseDTO update(String number, Contact contact) {
+        return null;
+    }
+
+    @Override
+    public MessageStatusResponseDTO delete(String number) {
+        return null;
+    }
+
+    @Override
+    public Boolean exists(String number) {
+        return null;
+    }
+}
