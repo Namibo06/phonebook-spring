2d57bb8ddc92c5a5b1dad701486812030139c545
Namibo06 <namibo654@gmail.com>
2024-07-26 13:15:09 -0300

    feat: add contract findContactByNameOrNumberPhone

--- src/main/java/com/waitomo/phonebook/infrastructure/persistence/JpaContactRepository.java
+++ src/main/java/com/waitomo/phonebook/infrastructure/persistence/JpaContactRepository.java
@@ -2,10 +2,17 @@ package com.waitomo.phonebook.infrastructure.persistence;
 
 import com.waitomo.phonebook.domain.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface JpaContactRepository extends JpaRepository<ContactEntity,Long>{
 
     Boolean existsByNumberPhone(String number);
+
+    @Query(value = "SELECT * FROM tb_contact WHERE name LIKE %:value% OR number_phone LIKE %:value%", nativeQuery = true)
+    Optional<ContactEntity> findContactByNameOrNumberPhone(@Param("value") String nameOrNumber);
 }
