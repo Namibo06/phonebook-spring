f3f5b8482af50b29fcdbd716aacc080c6bba4fe4
Namibo06 <namibo654@gmail.com>
2024-07-26 11:17:53 -0300

    feat: Added new annotation and created findAllContacts method

--- src/main/java/com/waitomo/phonebook/infrastructure/web/controllers/ContactController.java
+++ src/main/java/com/waitomo/phonebook/infrastructure/web/controllers/ContactController.java
@@ -2,25 +2,34 @@ package com.waitomo.phonebook.infrastructure.web.controllers;
 
 import com.waitomo.phonebook.application.mapper.ContactMapper;
 import com.waitomo.phonebook.application.usecases.CreateContactUseCase;
+import com.waitomo.phonebook.application.usecases.FindAllContactUseCase;
 import com.waitomo.phonebook.domain.entities.Contact;
 import com.waitomo.phonebook.infrastructure.persistence.ContactEntity;
+import com.waitomo.phonebook.infrastructure.web.responses.ContactResponse;
 import com.waitomo.phonebook.infrastructure.web.responses.MessageStatusResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 @RestController
 @RequestMapping("/contacts")
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ContactController {
     @Autowired
     private CreateContactUseCase createContactUseCase;
+
+    @Autowired
+    private FindAllContactUseCase findAllContactUseCase;
 
     @PostMapping
     public ResponseEntity<MessageStatusResponse> createContact(@RequestBody @Valid Contact contactRequest, UriComponentsBuilder uriBuilder) {
@@ -33,5 +42,12 @@ public class ContactController {
         MessageStatusResponse response = new MessageStatusResponse(MESSAGE_CREATED,STATUS_CREATED);
 
         return ResponseEntity.created(path).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ContactResponse>> findAllContacts(@PageableDefault(size = 50) Pageable pageable){
+        Page<ContactResponse> contactsPage = findAllContactUseCase.execute(pageable);
+
+        return ResponseEntity.ok(contactsPage);
     }
 }
