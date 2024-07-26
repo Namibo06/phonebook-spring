e3fe8f7872b706aa3abb4501ba04986d4a5167ad
Namibo06 <namibo654@gmail.com>
2024-07-25 23:24:26 -0300

    feat: created a contract to check if a contact based on a number exists

--- src/main/java/com/waitomo/phonebook/infrastructure/persistence/JpaContactRepository.java
+++ src/main/java/com/waitomo/phonebook/infrastructure/persistence/JpaContactRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaContactRepository extends JpaRepository<Contact,Long>{
+public interface JpaContactRepository extends JpaRepository<ContactEntity,Long>{
 
+    Boolean existsByNumberPhone(String number);
 }
