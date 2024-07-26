1314f4bc817d35b80b78e25149c35e9443d614d5
Namibo06 <namibo654@gmail.com>
2024-07-26 11:23:24 -0300

    refactor: new return in modelMapper

--- src/main/java/com/waitomo/phonebook/infrastructure/configuration/ModelMapperConfig.java
+++ src/main/java/com/waitomo/phonebook/infrastructure/configuration/ModelMapperConfig.java
@@ -1,6 +1,12 @@
 package com.waitomo.phonebook.infrastructure.configuration;
 
+import com.waitomo.phonebook.infrastructure.persistence.ContactEntity;
+import com.waitomo.phonebook.infrastructure.web.responses.ContactResponse;
+import org.hibernate.property.access.spi.Setter;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +15,18 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        Converter<ContactEntity, ContactResponse> contactConverter = new Converter<ContactEntity, ContactResponse>() {
+            @Override
+            public ContactResponse convert(MappingContext<ContactEntity, ContactResponse> context) {
+                ContactEntity source = context.getSource();
+                return new ContactResponse(source.getName(),source.getNumberPhone());
+            }
+        };
+
+        modelMapper.addConverter(contactConverter);
+
+        return modelMapper;
     }
 }
