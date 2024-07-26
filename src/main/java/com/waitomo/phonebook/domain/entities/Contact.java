f02db6646f08137242e7f79443fa75a34bef82ff
Namibo06 <namibo654@gmail.com>
2024-07-26 12:14:40 -0300

    refactor: remove id,setters,equals and hashCode

--- src/main/java/com/waitomo/phonebook/domain/entities/Contact.java
+++ src/main/java/com/waitomo/phonebook/domain/entities/Contact.java
@@ -1,56 +1,19 @@
 package com.waitomo.phonebook.domain.entities;
 
-import java.util.Objects;
-
 public class Contact {
-    private Long id;
-    private String name;
-    private String numberPhone;
+    private final String name;
+    private final String numberPhone;
 
-    public Contact(Long id, String name, String numberPhone) {
-        this.id = id;
+    public Contact(String name, String numberPhone) {
         this.name = name;
         this.numberPhone = numberPhone;
-    }
-
-    public Contact() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getNumberPhone() {
         return numberPhone;
-    }
-
-    public void setNumberPhone(String numberPhone) {
-        this.numberPhone = numberPhone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Contact contact = (Contact) o;
-        return Objects.equals(id, contact.id) && Objects.equals(name, contact.name) && Objects.equals(numberPhone, contact.numberPhone);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, numberPhone);
     }
 }
