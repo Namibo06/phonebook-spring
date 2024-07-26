b1c2a4a82a28b2a1636ce04cbdc112d746b51be2
Namibo06 <namibo654@gmail.com>
2024-07-25 23:29:50 -0300

    refactor: change type from Contact to ContactEntity

--- src/main/java/com/waitomo/phonebook/infrastructure/web/controllers/ContactController.java
+++ src/main/java/com/waitomo/phonebook/infrastructure/web/controllers/ContactController.java
@@ -1,9 +1,10 @@
 package com.waitomo.phonebook.infrastructure.web.controllers;
 
+import com.waitomo.phonebook.application.mapper.ContactMapper;
 import com.waitomo.phonebook.application.usecases.CreateContactUseCase;
 import com.waitomo.phonebook.domain.entities.Contact;
-import com.waitomo.phonebook.infrastructure.web.dtos.ContactRequestDTO;
-import com.waitomo.phonebook.infrastructure.web.dtos.MessageStatusResponseDTO;
+import com.waitomo.phonebook.infrastructure.persistence.ContactEntity;
+import com.waitomo.phonebook.infrastructure.web.responses.MessageStatusResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,14 +23,14 @@ public class ContactController {
     private CreateContactUseCase createContactUseCase;
 
     @PostMapping
-    public ResponseEntity<MessageStatusResponseDTO> createContact(@RequestBody @Valid ContactRequestDTO contactRequest, UriComponentsBuilder uriBuilder) {
-        Contact contact = createContactUseCase.execute(contactRequest.name(), contactRequest.numberPhone());
+    public ResponseEntity<MessageStatusResponse> createContact(@RequestBody @Valid Contact contactRequest, UriComponentsBuilder uriBuilder) {
+        ContactEntity contact = createContactUseCase.execute(ContactMapper.toEntity(contactRequest));
         Long contactId = contact.getId();
         URI path = uriBuilder.path("/contacts/{id}").buildAndExpand(contactId).toUri();
 
         final String MESSAGE_CREATED = "Contato criado";
         final Integer STATUS_CREATED = 201;
-        MessageStatusResponseDTO response = new MessageStatusResponseDTO(MESSAGE_CREATED,STATUS_CREATED);
+        MessageStatusResponse response = new MessageStatusResponse(MESSAGE_CREATED,STATUS_CREATED);
 
         return ResponseEntity.created(path).body(response);
     }
