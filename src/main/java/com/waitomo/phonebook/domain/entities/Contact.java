a69a497719edd17f2e6f08579fafe5ecc4b95b1a
Namibo06 <namibo654@gmail.com>
2024-07-25 23:30:29 -0300

    refactor: following Clean Architecture rules

--- src/main/java/com/waitomo/phonebook/domain/entities/Contact.java
+++ src/main/java/com/waitomo/phonebook/domain/entities/Contact.java
@@ -1,25 +1,56 @@
 package com.waitomo.phonebook.domain.entities;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import java.util.Objects;
 
-@Entity
-@Table(name = "tb_contact")
-@RequiredArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class Contact {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(length = 30,nullable = false)
     private String name;
-
-    @Column(name = "number_phone",unique = true,columnDefinition = "CHAR(11)",nullable = false)
     private String numberPhone;
+
+    public Contact(Long id, String name, String numberPhone) {
+        this.id = id;
+        this.name = name;
+        this.numberPhone = numberPhone;
+    }
+
+    public Contact() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumberPhone() {
+        return numberPhone;
+    }
+
+    public void setNumberPhone(String numberPhone) {
+        this.numberPhone = numberPhone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(id, contact.id) && Objects.equals(name, contact.name) && Objects.equals(numberPhone, contact.numberPhone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, numberPhone);
+    }
 }
