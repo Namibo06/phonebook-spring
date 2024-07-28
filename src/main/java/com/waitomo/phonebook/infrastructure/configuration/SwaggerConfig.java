37257954f32c6182dcb85e9e7d4f9e7cc78d3194
Namibo <namibo654@gmail.com>
2024-07-27 22:45:21 -0300

    refactor: create server

--- src/main/java/com/waitomo/phonebook/infrastructure/configuration/SwaggerConfig.java
+++ src/main/java/com/waitomo/phonebook/infrastructure/configuration/SwaggerConfig.java
@@ -2,12 +2,17 @@ package com.waitomo.phonebook.infrastructure.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "Agenda Telefônica",description = "API RESTFull de uma Agenda Telefônica com Clean Arquitecture",version = "1.0.0")
+        info = @Info(title = "Agenda Telefônica",description = "API RESTFull de uma Agenda Telefônica com Clean Arquitecture",version = "1.0.0"),
+        servers = {
+                @Server(url="http://0.0.0.0:8089",description = "Docker")
+        }
 )
 public class SwaggerConfig {
 }
