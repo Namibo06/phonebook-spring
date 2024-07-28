915102a8741e3954c6dede2f56e372bf6e9e57a1
Namibo <namibo654@gmail.com>
2024-07-27 22:38:24 -0300

    refactor: change from record to class with validations

--- src/main/java/com/waitomo/phonebook/infrastructure/web/responses/ContactRequest.java
+++ src/main/java/com/waitomo/phonebook/infrastructure/web/responses/ContactRequest.java
@@ -4,15 +4,42 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
-public record ContactRequest(
-        @NonNull
-        @NotBlank
-        @Size(max = 30, message = "Nome deve ter no máximo 30 caracteres")
-        String name,
-        @NonNull
-        @NotBlank
-        @Size(min = 11, max = 11, message = "Número de telefone deve ter exatamente 11 caracteres")
-        String numberPhone
-){
+public class ContactRequest {
+        private String name;
+        private String phoneNumber;
 
+        public ContactRequest() {}
+
+        public ContactRequest(String name, String phoneNumber) {
+                //colocar verificações para valid
+                if(name.isBlank() || phoneNumber.isBlank()){
+                        throw new RuntimeException("Valores em branco não permitido");
+                }
+                if(name.length() < 3 || name.length() > 30){
+                        throw new RuntimeException("Nome deve ter entre 3 e 30 caracteres");
+                }
+
+                if(phoneNumber.length() != 11){
+                        throw new RuntimeException("Um número do formato (99) 9 9999-9999 deve conter 11 números");
+                }
+
+                this.name = name;
+                this.phoneNumber = phoneNumber;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getPhoneNumber() {
+                return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+                this.phoneNumber = phoneNumber;
+        }
 }
